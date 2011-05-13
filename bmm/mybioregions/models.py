89fe613cad57e293450c220d7dcefcf09d8582c2
sfletche <sfletche@gmail.com>
2011-05-13 09:31:15 -0700

    overriding metanavigation block in map_ext

---
+++ bmm/mybioregions/models.py
@@ -0,0 +1,105 @@
+from django.conf import settings
+from django.db import models
+from django.contrib.gis.db import models
+from lingcod.features.models import PolygonFeature, FeatureCollection
+from lingcod.analysistools.models import Analysis
+from lingcod.features import register
+from lingcod.common.utils import asKml
+
+@register
+class MyBioregion(Analysis):
+    
+    #Input Parameters
+    input_start_point = models.TextField(verbose_name='Population Center')
+    input_temp_weight = models.FloatField(verbose_name='Value given to Temperature')
+    input_language_weight = models.FloatField(verbose_name='Value given to Spoken Language')
+    input_precip_weight = models.FloatField(verbose_name='Value given to Precipitation')
+    input_biomass_weight = models.FloatField(verbose_name='Value given to Vegetation')
+    
+    #Descriptors (name field is inherited from Analysis)
+    description = models.TextField(null=True, blank=True)
+    
+    # All output fields should be allowed to be Null/Blank
+    output_geom = models.PolygonField(srid=settings.GEOMETRY_DB_SRID,
+            null=True, blank=True, verbose_name="Bioregion Geometry")
+    
+    def run(self):
+        import time
+        time.sleep(5);
+        placeholder_name = self.input_start_point + ' placeholder'
+        placeholder = Placeholder.objects.get(name=placeholder_name)
+        self.output_geom = placeholder.geometry_final
+        return True
+        
+    @property 
+    def kml_working(self):
+        return """
+        <Placemark id="%s">
+            <visibility>0</visibility>
+            <name>%s (WORKING)</name>
+        </Placemark>
+        """ % (self.uid, self.name)
+
+    @property 
+    def kml_done(self):
+        return """
+        %s
+        <Placemark id="%s">
+            <visibility>1</visibility>
+            <name>%s</name>
+            <styleUrl>#%s-default</styleUrl>
+            <MultiGeometry>
+            %s
+            %s
+            </MultiGeometry>
+        </Placemark>
+        """ % (self.kml_style, self.uid, self.name, self.model_uid(),
+            asKml(self.output_geom.transform(
+                    settings.GEOMETRY_CLIENT_SRID, clone=True)),
+            asKml(self.output_geom.transform(
+                    settings.GEOMETRY_CLIENT_SRID, clone=True)))
+    
+    @property
+    def kml_style(self):
+        return """
+        <Style id="%s-default">
+            <IconStyle>
+                <color>ffffffff</color>
+                <colorMode>normal</colorMode>
+                <scale>0.9</scale> 
+                <Icon> <href>http://maps.google.com/mapfiles/kml/paddle/wht-blank.png</href> </Icon>
+            </IconStyle>
+            <LabelStyle>
+                <color>ffffffff</color>
+                <scale>0.8</scale>
+            </LabelStyle>
+            <PolyStyle>
+                <color>778B1A55</color>
+            </PolyStyle>
+        </Style>
+        """ % (self.model_uid(),)
+    
+    class Options:
+        verbose_name = 'Bioregion'
+        form = 'mybioregions.forms.MyBioregionForm'
+        form_template = 'mybioregion/form.html'
+        show_template = 'mybioregion/show.html'
+
+@register
+class Folder(FeatureCollection):
+    class Options:
+        form = 'mybioregions.forms.FolderForm'
+        valid_children = (
+            'bmm.mybioregions.models.MyBioregion',
+            'bmm.mybioregions.models.Folder',
+            'bmm.mybioregions.models.Placeholder',
+        )
+              
+class Placeholder(PolygonFeature):
+    description = models.TextField(default="", null=True, blank=True)
+
+    class Options:
+        manipulators = []
+        verbose_name = 'Bioregion Placeholder'
+        form = 'mybioregions.forms.PlaceholderForm'
+        show_template = 'placeholder/show.html'
