7a169c84d3bd8ab0b7d852e2412d740d0b3a6e02
Matthew Perry <mperry@ecotrust.org>
2011-06-01 11:53:04 -0700

    ignore .log file

---
+++ bmm/mybioregions/forms.py
@@ -0,0 +1,44 @@
+from lingcod.features.forms import FeatureForm, SpatialFeatureForm
+from mybioregions.models import MyBioregion, Folder
+from lingcod.analysistools.widgets import SliderWidget
+from django import forms
+from models import *
+
+class MyBioregionForm(FeatureForm):
+    #might change the following to ModelChoiceField to pull city names from a model
+    #also, might use different strategy altogether that allows users to simply select a point on a map as the starting point
+    input_start_point = forms.ChoiceField(label="Population Center", 
+            choices=([  ('beijing', 'Beijing, China'), 
+                        ('cairo', 'Cairo, Egypt'), 
+                        ('cape town', 'Cape Town, South Africa'), 
+                        ('portland', 'Portland, Oregon'),
+                        ('rio', 'Rio de Janeiro, Brazil'),
+                        ('sydney', 'Sydney, Australia')]), 
+            initial='portland', required=True)
+    #input_starting_point = forms.PointField(label="Starting Point")
+    input_temp_weight = forms.FloatField(min_value=0, max_value=1.0, initial=0.5,
+            widget=SliderWidget(min=0,max=1,step=0.01),
+            label="Value given to Temperature")
+    input_language_weight = forms.FloatField(min_value=0, max_value=1.0, initial=0.5,
+            widget=SliderWidget(min=0,max=1,step=0.01),
+            label="Value given to Spoken Language")
+    input_precip_weight = forms.FloatField(min_value=0, max_value=1.0, initial=0.5,
+            widget=SliderWidget(min=0,max=1,step=0.01),
+            label="Value given to Precipitation")
+    input_biomass_weight = forms.FloatField(min_value=0, max_value=1.0, initial=0.5,
+            widget=SliderWidget(min=0,max=1,step=0.01),
+            label="Value given to Vegetation")
+        
+    class Meta(FeatureForm.Meta):
+        model = MyBioregion
+        exclude = list(FeatureForm.Meta.exclude)
+        for f in model.output_fields():
+            exclude.append(f.attname)
+
+class FolderForm(FeatureForm):
+    class Meta(FeatureForm.Meta):
+        model = Folder
+    
+class PlaceholderForm(SpatialFeatureForm):
+    class Meta(SpatialFeatureForm.Meta):
+        model = Placeholder
---
+++ bmm/settings.py
@@ -0,0 +1,36 @@
+# Django settings for bmm project.
+from lingcod.common.default_settings import *
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TIME_ZONE = 'America/Vancouver'
+ROOT_URLCONF = 'bmm.urls'
+
+GEOMETRY_DB_SRID = 54009
+#KML_SIMPLIFY_TOLERANCE = .0002 #overriding this setting to account for lat/lon db srid
+KML_ALTITUDEMODE_DEFAULT = 'clampToGround'
+
+TEMPLATE_DIRS = ( os.path.realpath(os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/')), )
+
+INSTALLED_APPS += ( 'mybioregions', 
+                    'analysis',
+                    'lingcod.analysistools', #necessary?
+                    'django.contrib.humanize',
+                    'lingcod.raster_stats'
+                  )
+
+COMPRESS_CSS['application']['source_filenames'] += (
+    'bmm/css/analysis_report.css',
+)
+
+# bioregions.labs.ecotrust.org 
+GOOGLE_API_KEY = 'ABQIAAAAn9EXtJkKOlQBGEfk28CiahQkv7FNjWKr_TVFL7EgIFo2pXN1jRRz4grU7hzFNhpS8coxw1Dz219KfQ'
+# bioregions.labs.ecotrust.org:8000
+#GOOGLE_API_KEY = 'BQIAAAAn9EXtJkKOlQBGEfk28CiahTSuF7m83X3uEERz7nj3jSiHAkiaxQeB-tWbTkStcVCW35ldeO0TwWa5g'
+
+LOG_FILE =  os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'bmm.log'))
+#LOG_FILE = '/usr/local/apps/bioregions/bmm.log'
+LOGFILE =  os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'bmm.log'))
+
+APP_NAME = "The Bioregion Discovery Tool"
+
+from settings_local import *
