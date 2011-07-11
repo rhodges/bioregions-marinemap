50f184cc321014264f9d69fc01f9c0dcdbbe4896
Matthew Perry <mperry@ecotrust.org>
2011-07-11 08:33:36 -0700

    move desc down in the inpuyt form

---
+++ bmm/analysis/summary/summary_analysis.py
@@ -0,0 +1,217 @@
+#from django.http import HttpResponse
+from django.shortcuts import render_to_response#, get_object_or_404
+from django.template import RequestContext
+from lingcod.raster_stats.models import RasterDataset, zonal_stats
+#from analysis.models import *
+#from settings import *
+from lingcod.unit_converter.models import geometry_area_in_display_units, convert_float_to_area_display_units
+from analysis.models import Languages, EcoRegions, LastWild
+from analysis.caching.report_caching import *
+from lingcod.common.utils import clean_geometry
+
+default_value = '---'
+global_landmass = 148940000. #sq km -- see table at http://en.wikipedia.org/wiki/Earth
+npp_grid_cell_size = 676000000. #sq m -- approximated via measurement tool in arcmap 
+global_npp = 62000000000000000
+
+
+'''
+Runs analysis for Summary report
+Called by analysis.views.summary_analysis
+'''
+def display_summary_analysis(request, bioregion, template='summary_report.html'):
+    context = run_summary_analysis(bioregion)
+    return render_to_response(template, RequestContext(request, context)) 
+     
+'''
+Run the analysis, create the cache, and return the results as a context dictionary so they may be rendered with template
+'''    
+def run_summary_analysis(bioregion):
+    #get size of bioregion
+    area = get_size(bioregion)
+    #get current population (for 2010)
+    population = get_population(bioregion)
+    #get projected population (for 2015)
+    population_2015 = get_projected_population(bioregion)
+    #get list of spoken languages
+    languages = get_languages(bioregion)
+    #get median max temperature
+    max_temp_c, max_temp_f = get_max_temp(bioregion)
+    #get median min temperature
+    min_temp_c, min_temp_f = get_min_temp(bioregion)
+    #get mean annual temperature
+    annual_temp_c, annual_temp_f = get_annual_temp(bioregion)
+    #get mean annual temperature range
+    annual_temp_range_c, annual_temp_range_f = get_annual_temp_range(bioregion)
+    #get mean annual precipitation
+    annual_precip = get_annual_precip(bioregion)
+    #get existing eco-regions
+    ecoregions = get_ecoregions(bioregion)
+    #get last of the wild ecoregions 
+    wild_regions = get_last_wild(bioregion)
+    #get land mass proportion
+    landmass_perc = get_landmass_proportion(area)
+    #get npp proportion
+    npp_perc = get_npp_proportion(bioregion)
+    #get net primary production
+    avg_npp = get_avg_npp(bioregion)
+    #get soil suitability
+    soil_suitability = get_soil_suitability(bioregion)    
+    #compile context
+    context = {'bioregion': bioregion, 'default_value': default_value, 'area': area, 'population': population, 'population_2015': population_2015, 'languages': languages, 'max_temp_c': max_temp_c, 'max_temp_f': max_temp_f, 'min_temp_c': min_temp_c, 'min_temp_f': min_temp_f, 'annual_temp_c': annual_temp_c, 'annual_temp_f': annual_temp_f, 'annual_temp_range_c': annual_temp_range_c, 'annual_temp_range_f': annual_temp_range_f, 'annual_precip': annual_precip, 'ecoregions': ecoregions, 'wild_regions': wild_regions, 'landmass_perc': landmass_perc, 'soil_suitability': soil_suitability, 'avg_npp': avg_npp, 'npp_perc': npp_perc}
+    return context
+    #get average poverty index
+    #poverty = get_poverty(bioregion)
+    #get soil moisture
+    #soil_moisture = get_soil_moisture(bioregion)
+    
+           
+def get_size(bioregion):
+    area = int(round(geometry_area_in_display_units(bioregion.output_geom)))
+    return area
+           
+def get_population(bioregion):
+    pop_geom = RasterDataset.objects.get(name='population_2010')
+    pop_stats = zonal_stats(bioregion.output_geom, pop_geom)
+    return int(pop_stats.sum)
+
+def get_projected_population(bioregion):
+    pop_geom = RasterDataset.objects.get(name='population_2015')
+    pop_stats = zonal_stats(bioregion.output_geom, pop_geom)
+    return int(pop_stats.sum)
+   
+def get_languages(bioregion):
+    if report_cache_exists(bioregion, 'languages'):
+        language_names = get_report_cache(bioregion, 'languages')
+        return language_names
+    else:
+        languages = Languages.objects.all()
+        language_dict = {}
+        for language in languages:
+            try:
+                does_intersect = language.geometry.intersects(bioregion.output_geom)
+                if does_intersect:
+                    name = language.nam_ansi
+                    area = geometry_area_in_display_units(language.geometry.intersection(bioregion.output_geom))
+                    if name in language_dict.keys():
+                        language_dict[name] += area
+                    else:
+                        language_dict[name] = area
+            except:
+                #does_intersect = clean_geometry(language.geometry).intersects(bioregion.output_geom)
+                pass
+        #language_tuples = [(language.geometry.intersection(bioregion.output_geom).area, language.nam_ansi) for language in languages if clean_geometry(language.geometry).intersects(bioregion.output_geom)]
+        #language_dict = {}
+        #for area,name in language_tuples:
+        #    if name in language_dict.keys():
+        #        language_dict[name] += area
+        #    else:
+        #        language_dict[name] = area
+        language_tuples = [(area, name) for name,area in language_dict.items()]
+        language_tuples.sort(reverse=True)
+        language_names = [name for (area, name) in language_tuples]
+        create_report_cache(bioregion, dict(languages=language_names))
+        return language_names
+       
+def get_max_temp(bioregion):
+    max_temp_geom = RasterDataset.objects.get(name='max_temp')
+    max_temp_stats = zonal_stats(bioregion.output_geom, max_temp_geom)
+    max_temp_c = max_temp_stats.avg / 10
+    max_temp_f = max_temp_c * 9 / 5. + 32
+    return max_temp_c, max_temp_f
+   
+def get_min_temp(bioregion):
+    min_temp_geom = RasterDataset.objects.get(name='min_temp')
+    min_temp_stats = zonal_stats(bioregion.output_geom, min_temp_geom)
+    min_temp_c = min_temp_stats.avg / 10
+    min_temp_f = min_temp_c * 9 / 5. + 32
+    return min_temp_c, min_temp_f
+    
+def get_annual_temp(bioregion):
+    temp_geom = RasterDataset.objects.get(name='annual_temperature')
+    temp_stats = zonal_stats(bioregion.output_geom, temp_geom)
+    temp_c = temp_stats.avg / 10
+    temp_f = temp_c * 9 / 5. + 32
+    return temp_c, temp_f
+    
+def get_annual_temp_range(bioregion):
+    temp_range_geom = RasterDataset.objects.get(name='temp_range')
+    temp_range_stats = zonal_stats(bioregion.output_geom, temp_range_geom)
+    temp_range_c = temp_range_stats.avg / 10
+    temp_range_f = temp_range_c * 9 / 5. + 32
+    return temp_range_c, temp_range_f
+    
+def get_annual_precip(bioregion):
+    precip_geom = RasterDataset.objects.get(name='annual_precipitation')
+    precip_stats = zonal_stats(bioregion.output_geom, precip_geom)
+    return precip_stats.avg / 10
+    
+def get_ecoregions(bioregion):
+    if report_cache_exists(bioregion, 'ecoregions'):
+        ecoregion_tuples = get_report_cache(bioregion, 'ecoregions')
+        return ecoregion_tuples
+    else:
+        ecoregions = EcoRegions.objects.all()
+        ecoregion_tuples = [(ecoregion.geometry.intersection(bioregion.output_geom).area, ecoregion.eco_name) for ecoregion in ecoregions if ecoregion.geometry.intersects(bioregion.output_geom)]
+        ecoregion_dict = {}
+        for area,name in ecoregion_tuples:
+            if name in ecoregion_dict.keys():
+                ecoregion_dict[name] += area
+            else:
+                ecoregion_dict[name] = area
+        ecoregion_tuples = [(name, convert_float_to_area_display_units(area)) for name,area in ecoregion_dict.items()]
+        ecoregion_tuples.sort()
+        #ecoregion_names = [name for (area, name) in ecoregion_tuples]
+        create_report_cache(bioregion, dict(ecoregions=ecoregion_tuples))
+        #just return the following for now (until we get caching in place)
+        #ecoregion_names = ['Central and Southern Cascades forests', 'Central Pacific coastal forests', 'Willamette Valley forests', 'Puget lowland forests', 'Eastern Cascades forests', 'Klamath-Siskiyou forests', 'Snake-Columbia shrub steppe', 'Blue Mountains forests']
+        return ecoregion_tuples    
+    
+def get_last_wild(bioregion):
+    if report_cache_exists(bioregion, 'wild_regions'):
+        lastwild_tuples = get_report_cache(bioregion, 'wild_regions')
+        return lastwild_tuples
+    else:
+        wild_regions = LastWild.objects.all()
+        wild_region_tuples = [(wild_region.geometry.intersection(bioregion.output_geom).area, wild_region.eco_name) for wild_region in wild_regions if wild_region.geometry.intersects(bioregion.output_geom)]
+        wild_region_dict = {}
+        for area,name in wild_region_tuples:
+            if name in wild_region_dict.keys():
+                wild_region_dict[name] += area
+            else:
+                wild_region_dict[name] = area
+        wild_region_tuples = [(name, convert_float_to_area_display_units(area)) for name,area in wild_region_dict.items()]
+        wild_region_tuples.sort()
+        create_report_cache(bioregion, dict(wild_regions=wild_region_tuples))
+        return wild_region_tuples  
+    
+def get_landmass_proportion(area):
+    perc = area / global_landmass
+    return perc
+    
+def get_soil_suitability(bioregion):
+    suit_geom = RasterDataset.objects.get(name='soil_suitability')
+    suit_stats = zonal_stats(bioregion.output_geom, suit_geom)
+    return suit_stats.avg
+
+def get_npp_proportion(bioregion):
+    npp_geom = RasterDataset.objects.get(name='npp')
+    npp_stats = zonal_stats(bioregion.output_geom, npp_geom)
+    return npp_stats.sum / global_npp
+    
+def get_avg_npp(bioregion):
+    npp_geom = RasterDataset.objects.get(name='npp')
+    npp_stats = zonal_stats(bioregion.output_geom, npp_geom)
+    return npp_stats.avg / npp_grid_cell_size
+
+    
+def get_poverty(bioregion):
+    return default_value
+    poverty_geom = RasterDataset.objects.get(name='poverty')
+    poverty_stats = zonal_stats(bioregion.output_geom, poverty_geom)
+    return poverty_stats.avg
+
+def get_soil_moisture(bioregion):
+    soilmoist_geom = RasterDataset.objects.get(name='soil_moisture')
+    soilmoist_stats = zonal_stats(bioregion.output_geom, soilmoist_geom)
+    return soilmoist_stats.avg
---
+++ bmm/mybioregions/models.py
@@ -0,0 +1,207 @@
+from django.conf import settings
+from django.db import models
+from django.contrib.gis.db import models
+from django.utils.html import escape
+from lingcod.features.models import PolygonFeature, FeatureCollection
+from lingcod.analysistools.models import Analysis
+from lingcod.features import register
+from lingcod.common.utils import asKml
+import os
+import time
+
+
+@register
+class MyBioregion(Analysis):
+    
+    #Input Parameters
+    #input_start_point = models.TextField(verbose_name='Population Center')
+    input_starting_point = models.PointField(srid=settings.GEOMETRY_CLIENT_SRID, null=True, blank=True, verbose_name="Bioregion Starting Point")
+    input_temp_weight = models.FloatField(verbose_name='Value given to Temperature')
+    input_language_weight = models.FloatField(verbose_name='Value given to Spoken Language')
+    input_precip_weight = models.FloatField(verbose_name='Value given to Precipitation')
+    input_biomass_weight = models.FloatField(verbose_name='Value given to Vegetation')
+    
+    #Descriptors (name field is inherited from Analysis)
+    description = models.TextField(null=True, blank=True)
+    
+    # All output fields should be allowed to be Null/Blank
+    output_geom = models.PolygonField(srid=settings.GEOMETRY_DB_SRID,
+            null=True, blank=True, verbose_name="Bioregion Geometry")
+    
+    def run(self):
+        from lingcod.analysistools.grass import Grass
+
+        coords = self.input_starting_point.transform(settings.GEOMETRY_DB_SRID, clone=True)
+        max_cost=100 * (self.input_temp_weight + self.input_language_weight + \
+                        self.input_precip_weight + self.input_biomass_weight)
+
+        g = Grass('world_moll', 
+                gisbase="/usr/local/grass-6.4.1RC2", 
+                gisdbase="/home/grass",
+                autoclean=True)
+        g.verbose = True
+        g.run('g.region rast=soilmoist')
+        rasts = g.list()['rast']
+
+        outdir = '/tmp'
+        outbase = 'bioregion_%s' % str(time.time()).split('.')[0]
+        output = os.path.join(outdir,outbase+'.json')
+        if os.path.exists(output):
+            raise Exception(output + " already exists")
+
+        g.run('r.mapcalc "weighted_combined_slope =  0.01 + ' +
+                            '(%s * temp_slope) + ' % self.input_temp_weight  + 
+                            '(%s * lang_slope) + ' % self.input_language_weight  +
+                            '(%s * precip_slope) + ' % self.input_precip_weight +
+                            '(%s * biomass_slope)' % self.input_biomass_weight +
+                            '"')
+        g.run('r.rescale input=weighted_combined_slope output=wcr_slope to=0,100')
+        g.run('r.cost -k input=wcr_slope output=cost coordinate=%s,%s max_cost=%s' % \
+                (coords[0],coords[1],max_cost) )
+        g.run('r.mapcalc "bioregion=if(cost >= 0)"')
+        g.run('r.to.vect -s input=bioregion output=bioregion_poly feature=area')
+        g.run('v.out.ogr -c input=bioregion_poly type=area format=GeoJSON dsn=%s' % output)
+
+        from django.contrib.gis.gdal import DataSource
+        ds = DataSource(output)
+        layer = ds[0]
+        geom = layer[0].geom.geos
+
+        # Take the single polygon with the largest geometry 
+        # Assume the rest are slivers, etc
+        largest_area = geom.area
+        for feat in layer[1:]:
+            if feat.geom.area > largest_area:
+                largest_area = feat.geom.area
+                geom = feat.geom.geos
+
+        geom.srid = settings.GEOMETRY_DB_SRID 
+        g2 = geom.buffer(20000)
+        geom = g2.buffer(-20000)
+        if geom and not settings.DEBUG:
+            os.remove(output)
+            del g
+        self.output_geom = geom
+        return True
+        
+    def save(self, *args, **kwargs):
+        rerun = False
+        # only rerun the analysis if any of the input_ fields have changed
+        # ie if name and description change no need to rerun the full analysis
+        if self.pk is None:
+            rerun = True
+        else:
+            orig = MyBioregion.objects.get(pk=self.pk)
+            for f in MyBioregion.input_fields():
+                # Is original value different from form value?
+                if orig._get_FIELD_display(f) != getattr(self,f.name):
+                    rerun = True
+                    break
+        super(MyBioregion, self).save(rerun=rerun, *args, **kwargs) 
+
+    @classmethod
+    def mapnik_geomfield(self):
+        return "output_geom"
+
+    @classmethod
+    def mapnik_style(self):
+        import mapnik
+        polygon_style = mapnik.Style()
+        ps = mapnik.PolygonSymbolizer(mapnik.Color('#ffffff'))
+        ps.fill_opacity = 0.5
+        ls = mapnik.LineSymbolizer(mapnik.Color('#555555'),0.75)
+        ls.stroke_opacity = 0.5
+        r = mapnik.Rule()
+        r.symbols.append(ps)
+        r.symbols.append(ls)
+        polygon_style.rules.append(r)
+        return polygon_style
+
+    @property 
+    def kml_working(self):
+        return """
+        <Placemark id="%s">
+            <visibility>0</visibility>
+            <name>%s (WORKING)</name>
+        </Placemark>
+        """ % (self.uid, escape(self.name))
+
+    @property 
+    def kml_done(self):
+        return """
+        %s
+        <Placemark id="%s">
+            <visibility>1</visibility>
+            <name>%s</name>
+            <styleUrl>#%s-default</styleUrl>
+            <MultiGeometry>
+            %s
+            %s
+            </MultiGeometry>
+        </Placemark>
+        """ % (self.kml_style, self.uid, escape(self.name), self.model_uid(),
+            asKml(self.output_geom.transform(
+                    settings.GEOMETRY_CLIENT_SRID, clone=True)),
+            asKml(self.input_starting_point.transform(
+                    settings.GEOMETRY_CLIENT_SRID, clone=True)))
+    
+    @property
+    def kml_style(self):
+        return """
+        <Style id="%s-default">
+            <IconStyle>
+                <color>ffffffff</color>
+                <colorMode>normal</colorMode>
+                <scale>0.9</scale> 
+                <Icon> <href>http://maps.google.com/mapfiles/kml/paddle/wht-blank.png</href> </Icon>
+            </IconStyle>
+            <LabelStyle>
+                <color>ffffffff</color>
+                <scale>0.8</scale>
+            </LabelStyle>
+            <PolyStyle>
+                <color>778B1A55</color>
+            </PolyStyle>
+        </Style>
+        """ % (self.model_uid(),)
+    
+    @property
+    def hash(self):
+        """"
+        We are only concerned with the geometry here
+        """
+        geom_wkt = "%s" % (self.output_geom.wkt)
+        return geom_wkt.__hash__()        
+        
+    
+    class Options:
+        verbose_name = 'Bioregion'
+        icon_url = 'bmm/img/regions.png'
+        form = 'mybioregions.forms.MyBioregionForm'
+        form_template = 'mybioregion/form.html'
+        show_template = 'mybioregion/show.html'
+
+@register
+class Folder(FeatureCollection):
+    #name field is inherited from FeatureCollection
+    description = models.TextField(null=True, blank=True)
+    
+    class Options:
+        form = 'mybioregions.forms.FolderForm'
+        #form_template = 'folder/form.html'
+        show_template = 'folder/show.html'
+        valid_children = (
+            'bmm.mybioregions.models.MyBioregion',
+            'bmm.mybioregions.models.Folder',
+            'bmm.mybioregions.models.Placeholder',
+        )
+       
+class Placeholder(PolygonFeature):
+    description = models.TextField(default="", null=True, blank=True)
+
+    class Options:
+        manipulators = []
+        verbose_name = 'Bioregion Placeholder'
+        form = 'mybioregions.forms.PlaceholderForm'
+        show_template = 'placeholder/show.html'
+      
